ba5fe813b550d7791200a9d9fde460c8870207f3
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2022-03-23 15:53:50 -0300

    config: project setup

---
+++ desafiospring/src/main/java/br/com/meli/desafiospring/DesafiospringApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.desafiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiospringApplication.class, args);
+	}
+
+}
