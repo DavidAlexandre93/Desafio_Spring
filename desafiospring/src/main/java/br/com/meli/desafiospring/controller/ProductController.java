d66a35505688df53f9f655266bc74fe66578bdac
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-23 18:06:48 -0300

    feat: created inputDTO and postController

--- desafiospring/src/main/java/br/com/meli/desafiospring/controller/ProductController.java
+++ desafiospring/src/main/java/br/com/meli/desafiospring/controller/ProductController.java
@@ -1,4 +1,22 @@
 package br.com.meli.desafiospring.controller;
 
+import br.com.meli.desafiospring.dto.InputDTO;
+import br.com.meli.desafiospring.dto.ProductDTO;
+import br.com.meli.desafiospring.entity.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Controller
 public class ProductController {
+
+    //ResponseEntity<List<ProductDTO>>
+    @PostMapping("/api/v1/insert-articles-request")
+    public ResponseEntity<Object> postProducts(@RequestBody InputDTO input) {
+        System.out.println(input.getArticles());
+        return ResponseEntity.ok(input);
+    }
 }
