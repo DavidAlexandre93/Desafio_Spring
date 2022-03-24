791d6631b5212ecbc7401f255ccbc2b8d7f7046b
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-24 14:33:34 -0300

    feat: created productJson

--- desafiospring/src/main/java/br/com/meli/desafiospring/service/ProductService.java
+++ desafiospring/src/main/java/br/com/meli/desafiospring/service/ProductService.java
@@ -2,14 +2,19 @@ package br.com.meli.desafiospring.service;
 
 import br.com.meli.desafiospring.dto.InputDTO;
 import br.com.meli.desafiospring.dto.ProductDTO;
+import br.com.meli.desafiospring.entity.Product;
+import br.com.meli.desafiospring.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
+    private ProductRepository productRepository;
 
-    public void createProducts() {
-        InputDTO existingData;
+    public void createProducts(InputDTO input) {
+        List<Product> existingData = productRepository.readFile();
+        List<Product> newProducts = input.getArticles();
+        newProducts.stream().forEach(a -> existingData.add(a));
     }
 }
