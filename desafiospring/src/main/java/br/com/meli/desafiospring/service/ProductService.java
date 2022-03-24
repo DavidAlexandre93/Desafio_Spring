7ee5171796e6442eb669bd2c237a98f45921e039
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-24 12:37:54 -0300

    feat: Add jackson depepndency for reading/writing json

--- desafiospring/src/main/java/br/com/meli/desafiospring/service/ProductService.java
+++ desafiospring/src/main/java/br/com/meli/desafiospring/service/ProductService.java
@@ -1,4 +1,14 @@
 package br.com.meli.desafiospring.service;
 
+import br.com.meli.desafiospring.dto.ProductDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    public List<ProductDTO> createProducts() {
+
+    }
 }
