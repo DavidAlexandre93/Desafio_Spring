20b9beff5c9860ba5a971d533737956e84794014
Maik Henrique <maik.pereira@mercadolivre.com>
2022-03-24 17:49:44 -0300

    feat: implementation of shopping cart logic
    - returns the corresponde products when proper products are sent thourgh the request and throws an exception when the product does not exists

--- desafiospring/src/main/java/br/com/meli/desafiospring/controller/ProductController.java
+++ desafiospring/src/main/java/br/com/meli/desafiospring/controller/ProductController.java
@@ -1,33 +1,37 @@
 package br.com.meli.desafiospring.controller;
 
 import br.com.meli.desafiospring.dto.InputDTO;
-import br.com.meli.desafiospring.dto.ProductDTO;
+import br.com.meli.desafiospring.dto.ProductPurchaseRequestDTO;
+import br.com.meli.desafiospring.dto.PurchaseRequestDTO;
 import br.com.meli.desafiospring.entity.Product;
+import br.com.meli.desafiospring.entity.ShoppingCart;
 import br.com.meli.desafiospring.service.ProductService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import java.util.*;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1")
 public class ProductController {
 
     private final ProductService productService;
+    private final ModelMapper modelMapper;
 
     /**
      * R005, R006, R007
      */
-    @GetMapping("/api/v1/articles")
-    public List<Product> retornaPorPreco(@RequestParam String category,
+    @GetMapping("/articles")
+    public List<?> retornaPorPreco(@RequestParam String category,
                                          @RequestParam Boolean freeShipping,
                                          @RequestParam Integer order) {
-        ProductService.p.apply(order);
-        return productService.findByCritirion(category, freeShipping, order);
+        return productService.findByCriteria(category, freeShipping, order);
     }
-    
-    @PostMapping("/api/v1/insert-articles-request")
-    public ResponseEntity<List<ProductDTO>> postProducts(@RequestBody InputDTO input) {
+
+    @PostMapping("/insert-articles-request")
+    public ResponseEntity<?> postProducts(@RequestBody InputDTO input) {
         try {
             return ResponseEntity.ok(productService.createProducts(input));
 
@@ -37,6 +41,12 @@ public class ProductController {
         }
     }
 
+    @PostMapping("/purchase-request")
+    public ResponseEntity<?> purchaseProducts(@RequestBody PurchaseRequestDTO purchaseRequestDTO) {
+        ShoppingCart shoppingCart = modelMapper.map(purchaseRequestDTO, ShoppingCart.class);
+        List<Product> soldProducts = productService.sellProducts(shoppingCart);
+        return ResponseEntity.ok(soldProducts);
+    }
 }
 
 
