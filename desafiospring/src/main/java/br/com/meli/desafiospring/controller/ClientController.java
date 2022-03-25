2493f056d3ab05cc97da02dc655240deb431ffcf
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-03-25 15:32:26 -0300

    update: use of modelmapper class on client controller and cleared client imports

--- desafiospring/src/main/java/br/com/meli/desafiospring/controller/ClientController.java
+++ desafiospring/src/main/java/br/com/meli/desafiospring/controller/ClientController.java
@@ -1,17 +1,13 @@
 package br.com.meli.desafiospring.controller;
 
-import br.com.meli.desafiospring.clientValidator.CPFValidator;
-import br.com.meli.desafiospring.clientValidator.ClientNameValidator;
-import br.com.meli.desafiospring.clientValidator.ClientStateValidator;
 import br.com.meli.desafiospring.dto.ClientInputValidationDTO;
 import br.com.meli.desafiospring.entity.Client;
 import br.com.meli.desafiospring.exception.ClientRegisteredException;
-import br.com.meli.desafiospring.exception.ValidacaoException;
 import br.com.meli.desafiospring.service.ClientService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +20,13 @@ import javax.validation.Valid;
 @RequestMapping("/api/v2")
 public class ClientController {
 
+    private final ModelMapper modelMapper;
     ClientService clientService;
 
     @PostMapping("/insert-client")
     public ResponseEntity<?> postClient(@Valid @RequestBody ClientInputValidationDTO input) {
         try {
-            Client client = new Client(input. getId(), input.getName(), input.getCpf(), input.getState());
+            Client client = modelMapper.map(input, Client.class);
             clientService.createClient(client);
             return ResponseEntity.status(HttpStatus.CREATED).build();
 
